5227689bd7ebdfc0b6630c0f6dffe29eccc6dc33
bahassi123 <>
2021-04-14 03:42:58 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+       
+//First Cookie
+    Sugar tree = new Sugar("tree", false);
+    tree.cutOutShape("tree", 10);
+    tree.areBaked(350, 12);
+    tree.isDecorated();
+
+//Second Cookie
+   Sugar aCookie = new Sugar();
+    aCookie.isDecorated();
+  }
+}
